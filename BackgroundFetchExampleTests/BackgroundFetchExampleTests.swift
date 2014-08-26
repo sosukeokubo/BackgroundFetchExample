571ab33d963a1394ca8241d0497d46efb3347210
Sosuke Okubo <mojage@gmail.com>
2014-08-27 00:37:46 +0900

    first commit

---
+++ BackgroundFetchExampleTests/BackgroundFetchExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  BackgroundFetchExampleTests.swift
+//  BackgroundFetchExampleTests
+//
+//  Created by 大久保 創介 on 2014/08/26.
+//  Copyright (c) 2014年 Sosuke Okubo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BackgroundFetchExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Background Fetch Example
+=========================
+
+This is a sample for learning the behavior of the background fetch of iOS.
